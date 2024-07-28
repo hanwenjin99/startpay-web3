ddad13e32fb45d4d232ac63506a0b1511edc718d
hanwenjin99 <hanwenjin99@gmail.com>
2024-07-28 20:09:36 +0800

    sdd

--- server/model/system/user_withdraw_order.go
+++ server/model/system/user_withdraw_order.go
@@ -7,27 +7,27 @@ import (
 type UserWithDrawOrder struct {
 	global.GVA_MODEL
 	MerchantId   int64  `json:"merchantId"  gorm:"column:merchantId" `
-	MerchantName string `json:"merchantName" gorm:"default:;comment:merchantName"`
-	BankId       string `json:"bankId" gorm:"default:;comment:bankId"`
-	BankTitle    string `json:"bankTitle"  gorm:"default:;comment:bankTitle"`
-	Currency     string `json:"currency" gorm:"default:;comment:currency"`
-	Chain        string `json:"chain"  gorm:"default:;comment:chain"`
+	MerchantName string `json:"merchantName" gorm:"column:merchantName"`
+	BankId       string `json:"bankId" gorm:"column:bankId"`
+	BankTitle    string `json:"bankTitle"  gorm:"column:bankTitle"`
+	Currency     string `json:"currency" gorm:"column:currency"`
+	Chain        string `json:"chain"  gorm:"column:chain"`
 
-	Amount        float64 `json:"amount"  gorm:"index;comment:amount" `
-	Fee           float64 `json:"fee" gorm:"default:;comment:fee"`
-	RemittanceFee float64 `json:"remittanceFee" gorm:"default:;comment:remittanceFee"`
-	TotalAmount   float64 `json:"totalAmount" gorm:"default:;comment:totalAmount"`
+	Amount        float64 `json:"amount"  gorm:"column:amount" `
+	Fee           float64 `json:"fee" gorm:"column:fee"`
+	RemittanceFee float64 `json:"remittanceFee" gorm:"column:remittanceFee"`
+	TotalAmount   float64 `json:"totalAmount" gorm:"column:totalAmount"`
 
-	Status             int    `json:"status"  gorm:"index;comment:status" `
-	StatusName         string `json:"statusName" gorm:"default:;comment:statusName"`
-	TxInfo             string `json:"txInfo" gorm:"default:;comment:txInfo"`
-	TxCertificationUrl string `json:"txCertificationUrl"  gorm:"default:;comment:txCertificationUrl"`
-	TxReference        string `json:"txReference" gorm:"default:;comment:txReference"`
-	InputNote          string `json:"inputNote"  gorm:"index;comment:inputNote" `
-	Supplier           string `json:"supplier" gorm:"default:;comment:supplier"`
-	AdminMemo          string `json:"adminMemo" gorm:"default:;comment:adminMemo"`
-	Memo               string `json:"memo" gorm:"default:;comment:memo"`
-	AssembleAddress    string `json:"assemble_address" gorm:"default:;comment:assemble_address"`
+	Status             int    `json:"status"  gorm:"column:status" `
+	StatusName         string `json:"statusName" gorm:"column:statusName"`
+	TxInfo             string `json:"txInfo" gorm:"column:txInfo"`
+	TxCertificationUrl string `json:"txCertificationUrl"  gorm:"column:txCertificationUrl"`
+	TxReference        string `json:"txReference" gorm:"column:txReference"`
+	InputNote          string `json:"inputNote"  gorm:"column:inputNote" `
+	Supplier           string `json:"supplier" gorm:"column:supplier"`
+	AdminMemo          string `json:"adminMemo" gorm:"column:adminMemo"`
+	Memo               string `json:"memo" gorm:"column:memo"`
+	AssembleAddress    string `json:"assemble_address" gorm:"column:assemble_address"`
 }
 
 func (UserWithDrawOrder) TableName() string {
